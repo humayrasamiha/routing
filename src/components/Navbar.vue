5e1e9ba4ecd4ce6f54c544c8a71767f1dcdc7e42
Humayra Samiha <samihajui25@gmail.com>
2019-07-23 21:59:21 +0600

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'About'}">About</router-link>
+      </li>
+    </ul>
+    <h2>User Profile</h2>
+    <ul>
+      <li v-for="(id,index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params:{user_id: id} }">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <h2>Navigation Controls</h2>
+    <ul>
+      <li>
+        <button @click="goBack">Go Back</button>
+      </li>
+      <li>
+        <button @click="goHome">Redirect to home</button>
+      </li>
+      <li>
+        <button @click="goForward">Go Forward</button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+    goForward() {
+      this.$router.go(+1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+li {
+  display: inline-block;
+  margin: 10px;
+}
+a.router-link-exact-active {
+  color: purple;
+}
+</style>
---
+++ src/components/ViewProfile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile for {{ user_id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ViewProfile",
+  data() {
+    return {
+      user_id: this.$route.params.user_id
+    };
+  },
+  methods: {
+    updateId() {
+      this.user_id = this.$route.params.user_id;
+    }
+  },
+  watch: {
+    $route: "updateId"
+  }
+};
+</script>
+
+<style scoped>
+</style>
